b21d0415ec2efce9aad6de097f868a2cdfcfa84a
Humayun Khan <humayun@shopify.com>
2014-10-09 23:00:30 -0400

    shopping list app
    Lesson 2 final assignment for Thinkful FEWD course.

---
+++ js/app.js
@@ -0,0 +1,39 @@
+$(document).ready( function() {
+
+	
+
+	//Adding item to list
+
+	$('#add-button').click(function() {
+		if ($('input#add-todo').val().trim().length == 0 ) {
+				alert("put something here");
+				$('input#add-todo').val("");
+		} 
+
+		else {
+				$('#list').append('<li class="new">' + $('input#add-todo').val() + '</li>' + '<button class="done">Got It</button>' + '<button class="remove">Trash</button>');
+				$('input#add-todo').val("");
+		
+		}
+	});
+
+	//Crossing Item Off
+
+	$('#list').on('click', '.done', function(e){
+		e.preventDefault();
+		$('li').last().toggleClass("new cross");
+		$(this).remove();
+	});
+
+	//Removing Item
+
+	$('#list').on('click', '.remove', function(e){
+		e.preventDefault();
+		$('li').last().remove();
+		$(this).remove();
+		$('.done').remove();
+	});
+
+});
+
+	// Validate text box, then add value in text box to list
